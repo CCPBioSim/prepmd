7ea1e59a532e875b8d124cb9febc6e9986dedc06
harryswift01 <harry.swift@stfc.ac.uk>
2026-06-08 13:07:40 +0100

    docs: update `conf.py` to include autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'prepmd'
-copyright = '2026, CCPBioSim'
-version = '1.0.0'
+project = "prepmd"
+copyright = "2026, CCPBioSim"
+author = "CCPBioSim"
+
+version = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,11 +28,16 @@ extensions = [
 ]
 
 autosummary_generate = True
+
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
+
 napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
@@ -38,5 +45,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
