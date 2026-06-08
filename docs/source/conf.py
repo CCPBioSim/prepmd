c954c138ea64bd4268e606b4bf8d852b9e890a77
harryswift01 <harry.swift@stfc.ac.uk>
2026-06-08 13:41:39 +0100

    docs: configure autodoc optional dependency mocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "prepmd"
 copyright = "2026, CCPBioSim"
 author = "CCPBioSim"
 
 version = "1.0.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autosummary",
@@ -21,29 +17,40 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.extlinks",
     "nbsphinx",
     "sphinx_copybutton",
 ]
 
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Autosummary / autodoc ---------------------------------------------------
+
 autosummary_generate = True
+
+autosummary_mock_imports = [
+    "openff",
+    "openff.toolkit",
+    "sklearn",
+]
 
 autodoc_member_order = "bysource"
 autodoc_typehints = "description"
+
+autodoc_mock_imports = [
+    "openff",
+    "openff.toolkit",
+    "sklearn",
+]
+
+# -- Napoleon docstring settings --------------------------------------------
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-templates_path = ["_templates"]
-exclude_patterns = []
-
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
 html_static_path = ["_static"]
